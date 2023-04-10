0fbe4907449fc920ddf0da225d06951753fe247e
Maciej Korolik <maciej.korolik@monterail.com>
2023-04-10 14:17:35 +0200

    add sidebar with metadata

--- src/next-chat.tsx
+++ src/next-chat.tsx
@@ -8,9 +8,17 @@ type Values = {
 
 export default function Command() {
   const [answer, setAnswer] = useState<string | null>(null);
+  const [sources, setSources] = useState<{ path: string; name: string }[]>([]);
   async function handleSubmit(values: Values) {
     const answer = await getAnswer(values.question);
-    setAnswer(answer);
+    setAnswer(answer.text);
+    const sources = answer.sourceDocuments.map((doc) => {
+      const path = doc.metadata.source.split(".")[0];
+      const [name] = path.split("/").slice(-1);
+      return { path, name };
+    });
+    const uniqueSources = [...new Map(sources.map((v) => [v.path, v])).values()];
+    setSources(uniqueSources);
   }
 
   return (
@@ -25,7 +33,24 @@ export default function Command() {
         <Form.Description text="Ask something about Next.js" />
         <Form.TextArea id="question" title="Question" placeholder="Type your question..." />
       </Form>
-      {answer && <Detail markdown={answer} />}
+      {answer && (
+        <Detail
+          markdown={answer}
+          navigationTitle="Answer"
+          metadata={
+            <Detail.Metadata>
+              {sources.map((source) => (
+                <Detail.Metadata.Link
+                  key={source.path}
+                  title="Source:"
+                  target={`https://nextjs.org/docs/${source.path}`}
+                  text={source.name}
+                />
+              ))}
+            </Detail.Metadata>
+          }
+        />
+      )}
     </>
   );
 }
--- src/utils/gpt.ts
+++ src/utils/gpt.ts
@@ -17,6 +17,11 @@ export async function getAnswer(question: string): any {
     // OpenAI recommends replacing newlines with spaces for best results
     const sanitizedQuestion = question.trim().replaceAll("\n", " ");
 
+    const advancedQuery = `
+    I will ask you a question abut Next.js. Please answer it as best you can based on the provided documentation. Provide answer in markdown format, include code examples if needed.
+    The question is: ${sanitizedQuestion}
+    `;
+
     const index = (await pinecone).Index(PINECONE_INDEX_NAME);
     /* create vectorstore*/
     const vectorStore = await PineconeStore.fromExistingIndex(new OpenAIEmbeddings({ openAIApiKey: openAiApiKey }), {
@@ -26,15 +31,14 @@ export async function getAnswer(question: string): any {
 
     const model = openai;
     // create the chain
-    const chain = VectorDBQAChain.fromLLM(model, vectorStore, { k: 1, returnSourceDocuments: true });
+    const chain = VectorDBQAChain.fromLLM(model, vectorStore, { k: 5, returnSourceDocuments: true });
 
     //Ask a question
     const response = await chain.call({
-      query: sanitizedQuestion,
+      max_k: 5,
+      query: advancedQuery,
     });
-
-    console.log("response", response);
-    return response.text;
+    return response;
   } catch (error) {
     console.log(error);
   }
